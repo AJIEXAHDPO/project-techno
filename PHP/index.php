a15d842d344f3ef6ce6eeddcf6701857292e46ba
AJIEXAHDPO <alesha.iv03@gmail.com>
2023-12-23 16:54:26 +0300

    added db connection

---
+++ PHP/index.php
@@ -0,0 +1,24 @@
+<?php
+
+#namespace 
+
+$host = "127.0.0.1";
+$user = "root";
+$pswd = "21ahg76yb";
+$db = "techno";
+$port = "3306";
+
+class Database {
+    private $mysqli;
+
+    public function __construct($host, $user, $pswd, $db, $port) {
+        $this->mysqli = new mysqli($host, $user, $pswd, $db, $port);
+    }
+
+    public function query ($data) {
+
+        return $this->mysqli->query($data);
+    }
+}
+
+$db = new Database($host, $user, $pswd, $db, $port);
