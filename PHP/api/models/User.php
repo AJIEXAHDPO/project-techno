35542827d6d151c9fe8c0a893899416b81203f21
AJIEXAHDPO <alesha.iv03@gmail.com>
2023-12-24 21:32:52 +0300

    added initial User model

---
+++ PHP/api/models/User.php
@@ -0,0 +1,22 @@
+<?php
+class User {
+    private $name;
+    private $surname;
+    private $nickname;
+    private $email;
+    private $password;
+
+    public function __construct () {
+        //from POST statement
+    }
+
+    /*public function Login {
+        Authentication::Registry
+    }*/
+
+    /*public function Registry () {
+        Authentication::Login
+    }*/
+    
+    /*public function */
+}
