defb47ca46b6d1596118177b7d51b05361c853d0
AJIEXAHDPO <alesha.iv03@gmail.com>
2023-12-25 05:27:00 +0300

    fixed CRPS error

--- PHP/index.php
+++ PHP/index.php
@@ -5,15 +5,20 @@ use App\Models\Category;
 
 $categ_name="laptops";
 
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Accept, X-PINGOTHER, Content-Type');
+
 try {
 
     $pdo = Connection::get()->connect();
-    echo "successfull connection\n";
+    # echo "successfull connection\n";
     $category = new Category($categ_name, $pdo);
 
-    $jsonData = json_encode($category->receiveCategories());
+    $jsonData = json_encode($category->getFullList());
     echo $jsonData;
-    file_put_contents("productList.json", $jsonData);
+    # file_put_contents("productList.json", $jsonData);
 
 }
 catch (\PDOException $e) {
--- src/components/CatalogPage.js
+++ src/components/CatalogPage.js
@@ -1,15 +1,34 @@
 import ProductCard from "@components/UI/ProductCard";
 import CatalogFilter from "@components/UI/CatalogFilter"
-import productList from "@data/productList.json";
+//import productList from "@data/productList.json";
 import {importImages} from "@functions";
+import {useState, useEffect} from "react";
 
 const CatalogPage = () => {
-  const imageImports = importImages(productList);
+  const [productList, setProduclList] = useState([]);
+  const [imageImports, setImageImports] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8000/catalog/", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=UTF8",
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      setImageImports(importImages(data));
+      setProduclList(data);
+      console.log(data);
+    })
+  }, []);
+  
+  //const imageImports = importImages(productList);
   
   return (
   <> 
   <CatalogFilter />
-  <div class="container catalog">
+  <div className="container catalog">
     {productList.map(
       product => <ProductCard
         id={product.id}
