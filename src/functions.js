596709ac7912704039bb731d92fa8a92c5e37598
AJIEXAHDPO <alesha.iv03@gmail.com>
2023-12-25 06:27:03 +0300

    fixed import issue with no-exists file

--- src/functions.js
+++ src/functions.js
@@ -1,10 +1,17 @@
 const importImages = (list) => {
   const placeholder = require("@images/14hq.jpeg");
   const imageImports = {};
-  list.forEach((elem) => {
-      imageImports[elem.img] = elem.img? require(`@images/${elem.img}`): placeholder;
-    }
-  );
+
+      list.forEach((elem) => {
+        try {
+          imageImports[elem.img] = elem.img? require(`@images/${elem.img}`): placeholder;
+        } catch (error) {
+          console.log(`Import Error: no ${elem.img} exists`);
+          imageImports[elem.img] = placeholder;
+        }
+      }
+    );
+  
   return imageImports;
 }
 
