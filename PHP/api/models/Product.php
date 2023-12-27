52ae29d657503ca082d3ede966e5e4745de8b447
AJIEXAHDPO <alesha.iv03@gmail.com>
2023-12-27 08:36:25 +0300

    added get Discounts Function

--- PHP/api/models/Product.php
+++ PHP/api/models/Product.php
@@ -1,13 +1,38 @@
 <?php
 
-class Product {
-    
-    #public function getFullInfo() : Array {
+use Core\Model;
 
-        #return $result;
+class Product extends Model
+{
+
+    public function __construct()
+    {
+        $this->connectDB();
+    }
+
+    /*public function getFullInfo() : Array {
+        $result = $this->db->query("")->fetchAll(\PDO::FETCH_ASSOC);
+        return $result;
+    }*/
+
+    #public function postToUserCart() {
+
     #}
 
-    #public function postToUserCard() {
-        
-    #}
+    public function getDisccounts($count = "*"): array
+    {
+        $query = "SELECT product.id,
+                    product.name, 
+                    product.price, 
+                    product.img 
+               FROM product 
+         INNER JOIN category ON category_id = category.id
+              WHERE product.old_price != 0
+           ORDER BY product.popularity DESC";
+
+        if ($count !== "*") $query .= "LIMIT $count";
+
+        $result = $this->db->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+        return $result;
+    }
 }
