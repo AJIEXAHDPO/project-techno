286b40b4259c01ecb408527861aa95fa82b8ecc7
AJIEXAHDPO <alesha.iv03@gmail.com>
2023-12-27 08:29:13 +0300

    added getting query url function

--- PHP/core/Router.php
+++ PHP/core/Router.php
@@ -23,18 +23,34 @@ class Router
         }
     }
 
-    private function createController ($path) 
+    private function createController($path)
     {
-        $result = 'App\\Controllers\\'. ucfirst($path . "Controller");
+        $result = 'App\\Controllers\\' . ucfirst($path . "Controller");
+        return $result;
+    }
+
+    private function getUrlQuery(): array
+    {
+        $result = [];
+        if (empty($_SERVER['REQUEST_URI'])) 
+            return $result;
+
+        $queryStr = parse_url($_SERVER['REQUEST_URI'])['query'];
+        $queryArray = explode("&", $queryStr);
+
+        foreach ($queryArray as $query) {
+            $exploded = explode("=", $query);
+            $result[$exploded[0]] = $exploded[1];
+        }
         return $result;
     }
 
     public function run()
     {
         $uri = $this->getURI();
-        foreach($this->routes as $path => $callback)
+        foreach($this->routes as $path => $callback) 
         {
-            if(preg_match("/^$path/", $uri))
+            if(preg_match("/^$path/", $uri)) 
             {
                 $controller = $this->createController($path);
                 $controllerObject = new $controller();
@@ -43,6 +59,4 @@ class Router
             }
         }
     }
-
-
 }
