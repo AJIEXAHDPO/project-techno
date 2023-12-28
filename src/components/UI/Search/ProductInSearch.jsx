c34ac5bf3d5c7461bc58b1815e18cf41cebb0872
AJIEXAHDPO <alesha.iv03@gmail.com>
2023-12-28 17:28:29 +0300

    fixed Search panel displaying

--- src/components/UI/Search/ProductInSearch.jsx
+++ src/components/UI/Search/ProductInSearch.jsx
@@ -1,17 +1,11 @@
-import {useState} from "react";
+const ProductInSearch = ({ id: prodId, img, name: prodName, price }) => {
 
-const ProductInSearch = ({id: prodId, img, name: prodName, price})=> {
-  
   return (
-    <div id={prodId} className="container order-product-card">
-      <div className="order-product-block">
-        <img src={img} alt="product" className="order-product-photo" />
-        <h3 className="order-product-name">{prodName}</h3>
-      </div>
-      <div className="order-product-block">
-        <div className="product-price">{price}$</div>
-      </div>
-    </div>
+    <a id={prodId} href={`/product?id=${prodId}`} className="container search-product-card">
+      <img src={img} alt="product" className="order-product-photo" />
+      <h3 className="order-product-name">{prodName}</h3>
+      <div className="product-price">{price}$</div>
+    </a>
   );
 }
 
--- src/components/UI/Search/SearchPanel.jsx
+++ src/components/UI/Search/SearchPanel.jsx
@@ -1,42 +1,57 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import ProductInSearch from "./ProductInSearch";
-import {importImages} from "@functions";
+import { importImages } from "@functions";
 
-const orderList = require("@data/orderList.json");
-
-const imageImports = importImages(orderList);
-
-const SearchPanel = (props)=> {
+const SearchPanel = (props) => {
   const [inputText, setInputText] = useState("");
-  
-  const filterResults = (list, template)=> {
+  const [searchList, setSearchList] = useState([]);
+  const [imageImports, setImageImports] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/catalog`, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=UTF8",
+      }
+    })
+      .then(response => response.json())
+      .then((data) => {
+        setImageImports(importImages(data));
+        setSearchList(data);
+      })
+  }, []);
+
+  const filterResults = (list, template) => {
     const result = list.filter(
-      (elem)=> elem.name.indexOf(template) > -1
-    ).map((elem)=> <ProductInSearch 
-        id={elem.id}
-        price={elem.price} 
-        name={elem.name}
-        img={imageImports[elem.img]}/>);
+      (elem) => elem.name.indexOf(template) > -1
+    ).map((elem) => <ProductInSearch
+      id={elem.id}
+      price={elem.price}
+      name={elem.name}
+      img={imageImports[elem.img]} />);
     if (result.length === 0) {
-      return <div style={{textAlign: "center", width: 100+"%"}}>No results for "{template}"</div>;
+      return <div style={{ textAlign: "center", width: 100 + "%" }}>No results for "{template}"</div>;
     }
     return result;
   }
-  
-  const results = filterResults(orderList, inputText);
-  
-  const searchChangeHandler = (e)=> {
+
+  const results = filterResults(searchList, inputText);
+
+  const searchChangeHandler = (e) => {
     setInputText(e.target.value);
   }
-  
+
   return (
     <div className="dropdown-menu-background flex">
       <div className="search-panel">
-        <input type="text" placeholder="Search" className="input-search" 
-          onChange = {searchChangeHandler} value={inputText}/>
-        {props.children}
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between", width: 100+"%"}}>
+          <input type="text" placeholder="Search" className="input-search"
+            onChange={searchChangeHandler} value={inputText} />
+          {props.children}
+        </div>
         {results}
-      </div>    
+      </div>
     </div>
   );
 }
