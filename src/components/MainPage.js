caf4559f8a850fdfbb233d3b3b68177fa24ac242
AJIEXAHDPO <alesha.iv03@gmail.com>
2023-12-29 16:58:38 +0300

    added request for discounts list

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,42 +7,49 @@ import { useState, useEffect } from "react";
 function MainPage() {
   const [brands, setBrands] = useState([]);
   const [brandImports, setBrandImports] = useState([]);
+  const [discounts, setDiscounts] = useState([]);
+  const [discountsImports, setDiscountsImports] = useState([]);
 
-  useEffect(()=> {
+
+  useEffect(() => {
     fetch("http://localhost:8000/brands/", {
-        method: "GET",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json; charset=UTF8",
-        }
-      })
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=UTF8",
+      }
+    })
       .then(response => response.json())
       .then(data => {
         setBrandImports(importImages(data));
         setBrands(data);
         console.log(data);
       })
-    }, []);
+  }, []);
 
-  const discounts = [
-    { id: 1, name: "Adapter usb-c to usb-a", price: 7, img: "14hq.jpeg" },
-    { id: 2, name: "Optical Mouse Razer deathadder", price: 220, img: "bd9d7ae69542c1e44e2a2be0710bd715.jpg" },
-    { id: 3, name: "Laptop Dell u34678ME i7 12700U", price: 7500, img: "belmsru_ytntbi2rsffedylnym8b_1643617906.jpg" },
-  ];
-  
-  const placeholder = require("@images/14hq.jpeg");
-  const imageImports = {};
-  discounts.forEach((elem)=> {
-    imageImports[elem.img] = elem.img? require(`@images/${elem.img}`): placeholder;
-  });
-  
+  useEffect(() => {
+    fetch("http://localhost:8000/discounts/", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=UTF8",
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setDiscountsImports(importImages(data));
+        setDiscounts(data);
+        console.log(data);
+      })
+  }, []);
+
   const posters = [
-    {id: "banner1", headerText: "Premium Devices", innerText: "iPhone 12Pro\n at an affordable price",},
-    {id: "banner2", headerText: "Build your PC", innerText: "Create optimal pc for youself with our support and price calculators.",},
-    {id: "banner3", headerText: "JBL 30% OFF", innerText: "Sound you can touch",},
-    {id: "banner4", headerText: "MacBook Air", innerText: "Perfect sollution for designers",},
+    { id: "banner1", headerText: "Premium Devices", innerText: "iPhone 12Pro\n at an affordable price", },
+    { id: "banner2", headerText: "Build your PC", innerText: "Create optimal pc for youself with our support and price calculators.", },
+    { id: "banner3", headerText: "JBL 30% OFF", innerText: "Sound you can touch", },
+    { id: "banner4", headerText: "MacBook Air", innerText: "Perfect sollution for designers", },
   ];
-  
+
   return (
     <>
       <div className="promo">
@@ -50,26 +57,26 @@ function MainPage() {
         <div className="promo-link">Go store</div>
       </div>
 
-      <Container 
-        title="Daily discounts" 
+      <Container
+        title="Daily discounts"
         className="offers">
         <button className="back"></button>
         {discounts.map(
-          (product)=> 
+          (product) =>
             <ProductCard
               id={product.id}
               name={product.name}
               price={product.price}
-              img={imageImports[product.img]}
+              img={discountsImports[product.img]}
             />)}
         <button className="forward"></button>
       </Container>
 
-      <Container 
+      <Container
         className="banners"
-        title="Companies we are working with">
+        title="Spetial offers">
         {posters.map(
-          (banner)=> 
+          (banner) =>
             <AdPoster
               id={banner.id}
               headerText={banner.headerText}
@@ -77,11 +84,11 @@ function MainPage() {
             />)}
       </Container>
 
-      <Container 
-        title="Companies we are working with" 
+      <Container
+        title="Companies we are working with"
         className="logos">
         {brands.map(
-          (brand)=> <img className="logo" src={brandImports[brand.img]} alt={brand.name}></img>)}
+          (brand) => <img className="logo" src={brandImports[brand.img]} alt={brand.name}></img>)}
       </Container>
     </>
   );
