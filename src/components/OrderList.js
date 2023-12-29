c56cf9e805303d5e42239bb68cc559ac15b1cb53
AJIEXAHDPO <alesha.iv03@gmail.com>
2023-12-29 19:14:39 +0300

    removed extra file

--- src/components/OrderList.js
+++
@@ -1,8 +0,0 @@
-const OrderList = [
-  {id: 1, name: "Product full name", img: "air13.jpg", price: 220, quantity: 2},
-  {id: 2, name: "Product full name", img: "air13.jpg", price: 220, quantity: 2},
-  {id: 3, name: "Product full name", img: "6219718788.jpg", price: 220, quantity: 2},
-  {id: 4, name: "Product full name", img: "6470407044.jpg", price: 220, quantity: 2},
-];
-
-export default OrderList;
