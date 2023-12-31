3d95c78cee098f108736786a6702328253d333f3
AJIEXAHDPO <alesha.iv03@gmail.com>
2023-12-31 05:59:53 +0300

    remastered DB

--- dump.sql
+++ dump.sql
@@ -1,19 +1,44 @@
--- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: techno
+-- Host: localhost    Database: techno
 -- ------------------------------------------------------
--- Server version	8.0.35-0ubuntu0.23.04.1
+-- Server version	8.0.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `brand`
+--
+
+DROP TABLE IF EXISTS `brand`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `brand` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `brand`
+--
+
+LOCK TABLES `brand` WRITE;
+/*!40000 ALTER TABLE `brand` DISABLE KEYS */;
+INSERT INTO `brand` VALUES (1,'LG','lg.png'),(2,'AMD','amd.png'),(3,'DELL','dell.png'),(4,'PHILIPS','philips.png'),(5,'Apple','apple.png'),(6,'SAMSUNG','samsung.png'),(7,'ASUS','asus.png'),(8,'ACER','acer.png'),(9,'Google','google.png'),(10,'MSI','msi.png'),(11,'GIGABYTE','gigabyte.png'),(12,'HP','hp.png'),(13,'INTEL','intel.png'),(14,'SONY','sony.png'),(15,'NVidia','nvidia.png');
+/*!40000 ALTER TABLE `brand` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `category`
@@ -25,6 +50,7 @@ DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
+  `TableName` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -35,61 +61,47 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'laptops'),(2,'phones'),(3,'cables and adapters'),(4,'computer mice'),(5,'keyboars');
+INSERT INTO `category` VALUES (1,'laptops','laptops'),(2,'phones','phones'),(3,'cables and adapters','cables_and_adapters'),(4,'computer mice','mice'),(5,'keyboars','keyboards');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `category_prop`
+-- Table structure for table `laptops`
 --
 
-DROP TABLE IF EXISTS `category_prop`;
+DROP TABLE IF EXISTS `laptops`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `category_prop` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `category_id` int NOT NULL,
+CREATE TABLE `laptops` (
+  `id` int NOT NULL,
+  `Storage Size` varchar(25) DEFAULT NULL,
+  `RAM` varchar(25) DEFAULT NULL,
+  `CPU` varchar(100) DEFAULT NULL,
+  `OS` varchar(100) DEFAULT NULL,
+  `Screen Resolution` varchar(45) DEFAULT NULL,
+  `Battarey` varchar(25) DEFAULT NULL,
+  `GPU` varchar(100) DEFAULT NULL,
+  `Screen Type` varchar(25) DEFAULT NULL,
+  `RAM Type` varchar(25) DEFAULT NULL,
+  `CPU Series` varchar(100) DEFAULT NULL,
+  `Screen Diagonal` varchar(25) DEFAULT NULL,
+  `Storage Type` varchar(25) DEFAULT NULL,
+  `GPU Series` varchar(100) DEFAULT NULL,
+  `GPU VRAM Size` varchar(45) DEFAULT NULL,
+  `GPU VRAM Type` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_categoryProp_category` (`category_id`),
-  CONSTRAINT `fk_categoryProp_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_product_id` FOREIGN KEY (`id`) REFERENCES `product` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `category_prop`
+-- Dumping data for table `laptops`
 --
 
-LOCK TABLES `category_prop` WRITE;
-/*!40000 ALTER TABLE `category_prop` DISABLE KEYS */;
-/*!40000 ALTER TABLE `category_prop` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `prod_prop`
---
-
-DROP TABLE IF EXISTS `prod_prop`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `prod_prop` (
-  `prod_id` int NOT NULL,
-  `category_prop_id` int NOT NULL,
-  `value` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`prod_id`,`category_prop_id`),
-  KEY `fk_prodProp_categoryProp` (`category_prop_id`),
-  CONSTRAINT `fk_prodProp_categoryProp` FOREIGN KEY (`category_prop_id`) REFERENCES `category_prop` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_prodProp_product` FOREIGN KEY (`prod_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `prod_prop`
---
-
-LOCK TABLES `prod_prop` WRITE;
-/*!40000 ALTER TABLE `prod_prop` DISABLE KEYS */;
-/*!40000 ALTER TABLE `prod_prop` ENABLE KEYS */;
+LOCK TABLES `laptops` WRITE;
+/*!40000 ALTER TABLE `laptops` DISABLE KEYS */;
+INSERT INTO `laptops` VALUES (3,'2 TB','16 GB','Intel Core i7-12700H','Windows 11 Pro','3840x2400','8h','GeForce RTX 3070 Ti Mobile','IPS','DDR5','Intel Core i7','16\'\'','SSD','GeForce RTX','8 GB','GDDR6'),(4,'256 GB','8 GB','Intel Pentium Silver N5000','Windows 10 Home','1360x768','4h','Intel UHD Graphics 600','TN+film','DDR4','Intel Pentium','15.6\'\'','SSD','Intel UHD Graphics','-','-'),(5,'1 TB','16 GB','Intel Core i7-1280P','Windows 11 Home Single Language','1920x1080','6h','GeForce RTX 3060 Mobile','IPS','DDR4','Intel Core i7','15.6\'\'','SSD','GeForce RTX','6 GB','GDDR6');
+/*!40000 ALTER TABLE `laptops` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -106,6 +118,10 @@ CREATE TABLE `product` (
   `quantity` int DEFAULT NULL,
   `category_id` int NOT NULL,
   `img` varchar(255) DEFAULT NULL,
+  `popularity` int DEFAULT '1',
+  `old_price` int DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `brand` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_product_category` (`category_id`),
   CONSTRAINT `fk_product_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
@@ -118,7 +134,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'Adapter usb-c to usb-a',7,200,3,'14hq.jpeg'),(2,'Optical Mouse Razer deathadder',220,200,4,'bd9d7ae69542c1e44e2a2be0710bd715.jpg'),(3,'Laptop Dell u34678ME i7 12700U',7500,200,1,'belmsru_ytntbi2rsffedylnym8b_1643617906.jpg'),(4,'Laptop HP Silver',1500,200,1,'6219718788.jpg'),(5,'Laptop MSI RedDragon',1500,200,1,'6470407044.jpg'),(6,'Laptop MSI RedDragon',1500,200,1,'orig (1).jpeg'),(7,'Laptop Apple Mackbook Air13 Late 2020',1500,200,1,'msi_gp66_leopard_10ue_gp66_10ue_226xes_2.jpg'),(8,'Laptop Apple Mackbook Air13 Late 2020',1500,200,1,'img3.jpeg'),(9,'Laptop Apple Mackbook Air13 Late 2020',1500,200,1,'air13.jpg'),(10,'Laptop Apple Macbook Air A2337',2400,200,1,'1151249-7.jpg'),(11,'Laptop ASUS Vivobook 15 X1504ZA-BQ824',1200,200,1,'6219718788.jpg'),(12,'Laptop Lenovo IdeaPad Slim 3 15IAN8',440,200,1,'orig (2).jpeg'),(13,'Laptop ASUS Vivobook 17 M1702QA-AU082',670,200,1,'2_asusvivobook_prom580gdfi496t1.jpg'),(14,'Laptop Lenovo IdeaPad 4 15IGL05',440,200,1,'x332_trim.png'),(15,'Laptop MSI Prestige 15 A12UD-225RU',1200,200,1,'x332_trim.png');
+INSERT INTO `product` VALUES (1,'Cable Xiaomi USB-C USB-A white 1.5m',7,200,3,'14hq.jpeg',4,10,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','Xiaomi'),(2,'Optical Mouse Razer deathadder',220,200,4,'bd9d7ae69542c1e44e2a2be0710bd715.jpg',3,250,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','Razer'),(3,'Laptop GIGABYTE AERO 16 XE5 Silver',925,200,1,'belmsru_ytntbi2rsffedylnym8b_1643617906.jpg',2,1449,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','GIGABYTE'),(4,'Laptop ASUS X540MA-GQ297',298,200,1,'6219718788.jpg',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','HP'),(5,'Laptop  Stealth 15M B12UE-011US',999,200,1,'6470407044.jpg',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','MSI'),(6,'Laptop HP',926,200,1,'orig (1).jpeg',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','MSI'),(7,'Laptop MSI Leopard GP76 i7-10870H',855,200,1,'msi_gp66_leopard_10ue_gp66_10ue_226xes_2.jpg',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','MSI'),(8,'Laptop Apple Mackbook Air13 Late 2020',1500,200,1,'img3.jpeg',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','MSI'),(9,'Laptop Apple Mackbook Air13 Late 2020',1500,200,1,'air13.jpg',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','Apple'),(10,'Laptop Apple Macbook Air A2337',2400,200,1,'1151249-7.jpg',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','Apple'),(11,'Laptop ASUS Vivobook 15 X1504ZA-BQ824',1200,200,1,'6219718788.jpg',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','ASUS'),(12,'Laptop Lenovo IdeaPad 3 15IGL05',270,200,1,'orig (2).jpeg',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','Lenovo'),(13,'Laptop ASUS Vivobook 17 M1702QA-AU082',670,200,1,'2_asusvivobook_prom580gdfi496t1.jpg',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','ASUS'),(14,'Laptop Lenovo IdeaPad Slim 3 15IAN8',440,200,1,'x332_trim.png',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','Lenovo'),(15,'Laptop MSI Prestige 15 A12UD-225RU',1200,200,1,'x332_trim.png',1,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','MSI');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -158,4 +174,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-25 15:53:31
+-- Dump completed on 2023-12-31  4:54:26
