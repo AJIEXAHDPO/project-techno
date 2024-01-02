6771cdb296a59100ad9a7515e382bfa2a7c9335b
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-01-02 22:38:07 +0300

    added import function for single image

--- src/functions.js
+++ src/functions.js
@@ -2,17 +2,30 @@ const importImages = (list) => {
   const placeholder = require("@images/no_image.jpg");
   const imageImports = {};
 
-      list.forEach((elem) => {
-        try {
-          imageImports[elem.img] = elem.img? require(`@images/${elem.img}`): placeholder;
-        } catch (error) {
-          console.log(`Import Error: no ${elem.img} exists`);
-          imageImports[elem.img] = placeholder;
-        }
-      }
-    );
-  
+  list.forEach((elem) => {
+    try {
+      imageImports[elem.img] = elem.img ? require(`@images/${elem.img}`) : placeholder;
+    } catch (error) {
+      console.log(`Import Error: no ${elem.img} exists`);
+      imageImports[elem.img] = placeholder;
+    }
+  }
+  );
+
   return imageImports;
 }
 
-export {importImages};
+const importImage = (img) => {
+  const placeholder = require("@images/no_image.jpg");
+  let imageImport;
+  try {
+    imageImport = img ? require(`@images/${img}`) : placeholder;
+  } catch (error) {
+    console.error(`Import Error: no ${img} exists`);
+    imageImport = placeholder;
+  }
+  return imageImport;
+}
+
+
+export { importImages, importImage };
