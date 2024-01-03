f3665af34ddc2f4466de676526e8acf3bd837a2a
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-01-03 15:59:56 +0300

    fixed routes

--- PHP/index.php
+++ PHP/index.php
@@ -13,9 +13,9 @@ header('Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, A
 $routes = [
     new Core\Route("GET", "/^catalog$/", "CatalogController", "getFullCategoryList"),
     new Core\Route("GET", "/^catalog\/\w+/", "CatalogController", "getFullList"),
-    new Core\Route("GET", "/^brands$/", "BrandsController", "getFullList"),
+    new Core\Route("GET", "/^brands$/", "BrandsController", "getList"),
     new Core\Route("GET", "/^product\?id=[1-9][0-9]*$/", "ProductController", "getFullInfo"),
-    new Core\Route("GET", "/^discounts$/", "DiscountsController", "getDiscountsList"),
+    new Core\Route("GET", "/^discounts$/", "CatalogController", "getDiscountsList"),
 ];
 
 $router = new Router($routes);
