4df78a8b3dcf6e8830e14aed46532b8ee5cc8cc2
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-01-04 23:57:25 +0300

    fixed routing

--- PHP/api/routes/Routes.php
+++ PHP/api/routes/Routes.php
@@ -9,9 +9,9 @@ function getRoutes()
     $routes = [
         new Route("GET", "/^catalog$/", "CatalogController", "getFullCategoryList"),
         new Route("GET", "/^catalog\/\w+/", "CatalogController", "getFullList"),
-        new Route("GET", "/^brands$/", "BrandsController", "getFullList"),
+        new Route("GET", "/^brands$/", "BrandsController", "getList"),
         new Route("GET", "/^product\?id=[1-9][0-9]*$/", "ProductController", "getFullInfo"),
-        new Route("GET", "/^discounts$/", "DiscountsController", "getDiscountsList"),
+        new Route("GET", "/^discounts$/", "CatalogController", "getDiscountsList"),
     ];
     return $routes;
 }
