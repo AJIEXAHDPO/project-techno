2784b73d4d282ea265310ba0d3f15df96ca03935
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-01-06 04:06:07 +0300

    added price rangebar logic

--- src/components/UI/Filter/PriceRangebar.jsx
+++ src/components/UI/Filter/PriceRangebar.jsx
@@ -1,18 +1,84 @@
 import { useEffect, useState } from "react";
 
 const PriceRangebar = ({ minPrice, maxPrice }) => {
+    const [priceMin, setPriceMin] = useState(minPrice);
+    const [priceMax, setPriceMax] = useState(maxPrice);
+    const [thumb1Position, setThumb1Position] = useState(0);
+    const [thumb2Position, setThumb2Position] = useState(345 - 7);
+
+    useEffect(() => {
+        const promise1 = new Promise(resolve => resolve());
+        const promise2 = new Promise(resolve => resolve());
+        const intervalElem = document.querySelector(".rangebar-interval");
+        const intervalWidth = thumb2Position - thumb1Position;
+        promise1
+            .then(() => document.querySelector(".r1"))
+            .then(thumb1 => thumb1.style.left = `${thumb1Position}px`);
+        promise2
+            .then(() => document.querySelector(".r2"))
+            .then(thumb2 => {
+                thumb2.style.left = thumb2Position + "px";
+                intervalElem.style.left = `${thumb1Position}px`;
+                intervalElem.style.width = `${intervalWidth}px`;
+            });
+    }, [thumb1Position, thumb2Position]);
     return (
         <div className="filter-section price-rangebar">
             <h3 className="filter-point">Price</h3>
             <div className="range">
                 <div className="rangebar-track">
-                    <div className="rangebar-thumb r1"></div>
+                    <div
+                        className="rangebar-thumb r1"
+                        onMouseDown={e => {
+                            document.ondragstart = null;
+                            const initialThumbPosition = e.pageX;
+                            document.onmousemove = (event) => {
+                                let calculatedPosition = thumb1Position + (event.pageX - initialThumbPosition);
+                                if (calculatedPosition < 0) calculatedPosition = 0;
+                                else if (calculatedPosition > thumb2Position - 7) calculatedPosition = thumb2Position - 7;
+                                setThumb1Position(calculatedPosition);
+                                setPriceMin(Math.trunc(minPrice + (maxPrice - minPrice) / (345 - 7) * calculatedPosition));
+                            }
+                            document.onmouseup = () => {
+                                document.onmousemove = null;
+                                document.onmouseup = null;
+                            }
+                        }}
+                    ></div>
                     <div className="rangebar-interval"></div>
-                    <div className="rangebar-thumb r2"></div>
+                    <div
+                        className="rangebar-thumb r2"
+                        onMouseDown={e => {
+                            document.ondragstart = null;
+                            const initialThumbPosition = e.pageX;
+                            document.onmousemove = (event) => {
+                                let calculatedPosition = thumb2Position + (event.pageX - initialThumbPosition);
+                                if (calculatedPosition > 345 - 7) calculatedPosition = 345 - 7;
+                                else if (calculatedPosition < thumb1Position + 7) calculatedPosition = thumb1Position + 7;
+                                setThumb2Position(calculatedPosition);
+                                setPriceMax(Math.trunc(minPrice + (maxPrice - minPrice) / (345 -7) * calculatedPosition));
+                            }
+                            document.onmouseup = () => {
+                                document.onmousemove = null;
+                                document.onmouseup = null;
+                            }
+                        }}
+                    ></div>
                 </div>
                 <div className="price-range">
-                    <input className="price-min" name="price-min" value={minPrice} />
-                    <input className="price-max" name="price-max" value={maxPrice} />
+                    <input
+                        type="number"
+                        className="price-min"
+                        onChange={e => setPriceMin(e.target.value)}
+                        name="price-min"
+                        value={priceMin}
+                    />
+                    <input
+                        type="number"
+                        className="price-max"
+                        onChange={e => setPriceMax(e.target.value)}
+                        name="price-max" value={priceMax}
+                    />
                 </div>
             </div>
         </div>
