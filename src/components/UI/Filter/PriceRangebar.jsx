8f9290c86656343d9e127eacd55e20ecf635a0fd
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-04-09 19:45:31 +0300

    remastered a catalog page styling

--- src/components/UI/Filter/PriceRangebar.jsx
+++ src/components/UI/Filter/PriceRangebar.jsx
@@ -1,17 +1,56 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const PriceRangebar = ({ minPrice, maxPrice }) => {
-    const rangebarWidth= 260;
-    const [priceMin, setPriceMin] = useState(minPrice);
-    const [priceMax, setPriceMax] = useState(maxPrice);
-    const [thumb1Position, setThumb1Position] = useState(0);
-    const [thumb2Position, setThumb2Position] = useState(rangebarWidth - 7);
+    const [rangebarWidth, setRangebarWidth] = useState(0);
+    // const rangebarWidth = 260;
+    const { search } = useLocation();
+    const q = new URLSearchParams(search);
+    const currentMax = q.get("price-max");
+    const currentMin = q.get("price-min");
+    const [priceMin, setPriceMin] = useState(currentMin ?? minPrice);
+    const [priceMax, setPriceMax] = useState(currentMax ?? maxPrice);
+    const [thumb1Position, setThumb1Position] = useState(rangebarWidth / (maxPrice - minPrice) * (priceMin - minPrice));
+    const [thumb2Position, setThumb2Position] = useState(rangebarWidth / (maxPrice - minPrice) * (priceMax - minPrice));
 
     useEffect(() => {
+        setRangebarWidth(document.querySelector(".rangebar-track").clientWidth - 14);
+    }, [])
+
+    useEffect(() => {
+        let min = currentMin ?? minPrice;
+        let max = currentMax ?? maxPrice;
+        if (currentMin && (currentMin > maxPrice)) min = maxPrice; // setPriceMin(maxPrice);
+        if (currentMin && (currentMin < minPrice)) min = minPrice; // setPRiceMin(minPrice);
+        if (currentMax && (currentMax > maxPrice)) max = maxPrice; // setPriceMax(maxPrice);
+        if (currentMax && (currentMax < minPrice)) max = minPrice; // setPriceMax(minPrice);
+
+        setPriceMin(min);
+        setPriceMax(max);
+        console.log("currentMin: ", currentMin);
+        console.log("maxPrice: ", maxPrice)
+        console.log(`thumb1Prosition = ${rangebarWidth} / (${maxPrice} - ${minPrice}) * (${min}) - ${minPrice}`);
+
+        if (maxPrice === minPrice) {
+            setThumb1Position(0);
+            setThumb2Position(rangebarWidth);
+        }
+        else {
+            setThumb1Position(rangebarWidth / (maxPrice - minPrice) * ((min) - minPrice));
+            setThumb2Position(rangebarWidth / (maxPrice - minPrice) * ((max) - minPrice));
+        }
+    }, [minPrice, maxPrice, rangebarWidth, currentMin, currentMax])
+
+    useEffect(() => {
+        console.log(`priceMin: ${priceMin}`);
+        console.log(`priceMax: ${priceMax}`);
+        console.log(`thumb 1 position: ${thumb1Position}`);
+        console.log(`thumb 2 position: ${thumb2Position}`);
         const promise1 = new Promise(resolve => resolve());
         const promise2 = new Promise(resolve => resolve());
         const intervalElem = document.querySelector(".rangebar-interval");
         const intervalWidth = thumb2Position - thumb1Position;
+        console.log("intervalWidth: ", intervalWidth);
         promise1
             .then(() => document.querySelector(".r1"))
             .then(thumb1 => thumb1.style.left = `${thumb1Position}px`);
@@ -22,7 +61,7 @@ const PriceRangebar = ({ minPrice, maxPrice }) => {
                 intervalElem.style.left = `${thumb1Position}px`;
                 intervalElem.style.width = `${intervalWidth}px`;
             });
-    }, [thumb1Position, thumb2Position]);
+    }, [thumb1Position, thumb2Position, priceMin, priceMax]);
     return (
         <div className="filter-section price-rangebar">
             <h3 className="filter-point">Price</h3>
@@ -38,7 +77,7 @@ const PriceRangebar = ({ minPrice, maxPrice }) => {
                                 if (calculatedPosition < 0) calculatedPosition = 0;
                                 else if (calculatedPosition > thumb2Position - 7) calculatedPosition = thumb2Position - 7;
                                 setThumb1Position(calculatedPosition);
-                                setPriceMin(Math.trunc(minPrice + (maxPrice - minPrice) / (rangebarWidth - 7) * calculatedPosition));
+                                setPriceMin(Math.trunc(minPrice + (maxPrice - minPrice) / (rangebarWidth) * calculatedPosition));
                             }
                             document.onmouseup = () => {
                                 document.onmousemove = null;
@@ -53,7 +92,7 @@ const PriceRangebar = ({ minPrice, maxPrice }) => {
                                 if (calculatedPosition < 0) calculatedPosition = 0;
                                 else if (calculatedPosition > thumb2Position - 7) calculatedPosition = thumb2Position - 7;
                                 setThumb1Position(calculatedPosition);
-                                setPriceMin(Math.trunc(minPrice + (maxPrice - minPrice) / (rangebarWidth - 7) * calculatedPosition));
+                                setPriceMin(Math.trunc(minPrice + (maxPrice - minPrice) / (rangebarWidth) * calculatedPosition));
                             }
                             document.ontouchend = () => {
                                 document.ontouchmove = null;
@@ -69,10 +108,10 @@ const PriceRangebar = ({ minPrice, maxPrice }) => {
                             const initialThumbPosition = e.pageX;
                             document.onmousemove = (event) => {
                                 let calculatedPosition = thumb2Position + (event.pageX - initialThumbPosition);
-                                if (calculatedPosition > rangebarWidth - 7) calculatedPosition = rangebarWidth - 7;
+                                if (calculatedPosition > rangebarWidth) calculatedPosition = rangebarWidth;
                                 else if (calculatedPosition < thumb1Position + 7) calculatedPosition = thumb1Position + 7;
                                 setThumb2Position(calculatedPosition);
-                                setPriceMax(Math.trunc(minPrice + (maxPrice - minPrice) / (rangebarWidth - 7) * calculatedPosition));
+                                setPriceMax(Math.trunc(minPrice + (maxPrice - minPrice) / (rangebarWidth) * calculatedPosition));
                             }
                             document.onmouseup = () => {
                                 document.onmousemove = null;
@@ -84,10 +123,10 @@ const PriceRangebar = ({ minPrice, maxPrice }) => {
                             const initialThumbPosition = e.touches[0].pageX;
                             document.ontouchmove = (event) => {
                                 let calculatedPosition = thumb2Position + (event.touches[0].pageX - initialThumbPosition);
-                                if (calculatedPosition > rangebarWidth - 7) calculatedPosition = rangebarWidth - 7;
+                                if (calculatedPosition > rangebarWidth) calculatedPosition = rangebarWidth;
                                 else if (calculatedPosition < thumb1Position + 7) calculatedPosition = thumb1Position + 7;
                                 setThumb2Position(calculatedPosition);
-                                setPriceMax(Math.trunc(minPrice + (maxPrice - minPrice) / (rangebarWidth - 7) * calculatedPosition));
+                                setPriceMax(Math.trunc(minPrice + (maxPrice - minPrice) / (rangebarWidth) * calculatedPosition));
                             }
                             document.ontouchend = () => {
                                 document.ontouchmove = null;
@@ -100,15 +139,30 @@ const PriceRangebar = ({ minPrice, maxPrice }) => {
                     <input
                         type="number"
                         className="price-min"
-                        onChange={e => setPriceMin(e.target.value)}
+                        onChange={e => {
+                            if (e.target.value > maxPrice) e.target.value = maxPrice;
+                            if (e.target.value > priceMax) e.target.value = priceMax;
+                            if (e.target.value < minPrice) e.target.value = minPrice;
+                            setPriceMin(e.target.value);
+                            setThumb1Position(rangebarWidth / (maxPrice - minPrice) * (e.target.value - minPrice));
+                        }}
                         name="price-min"
+                        min={0}
                         value={priceMin}
                     />
                     <input
                         type="number"
                         className="price-max"
-                        onChange={e => setPriceMax(e.target.value)}
-                        name="price-max" value={priceMax}
+                        onChange={e => {
+                            if (e.target.value > maxPrice) e.target.value = maxPrice;
+                            if (e.target.value < minPrice) e.target.value = minPrice;
+                            if (e.target.value < priceMin) e.target.value = priceMin;
+                            setPriceMax(e.target.value);
+                            setThumb2Position(rangebarWidth / (maxPrice - minPrice) * (e.target.value - minPrice)) //
+                        }}
+                        name="price-max"
+                        max={maxPrice}
+                        value={priceMax}
                     />
                 </div>
             </div>
--- src/components/UI/ProductCard.js
+++ src/components/UI/ProductCard.js
@@ -5,17 +5,20 @@ function ProductCard(props) {
         <img src={props.img} alt={props.name} className="product-photo" />
       </div>
       <div className="product-photo-filter"></div>
-      <div className="product-description">
-        <div className="product-name">{props.name}</div>
-        <div className="rating">
-          <span>rating</span>
-          <div className="star"></div>
-          <div className="star"></div>
-          <div className="star"></div>
-          <div className="star"></div>
-          <div className="star"></div>
+      <div>
+        <div className="product-description">
+          <div className="price">{props.price}{props.price < 1000 ? ".00" : ""} $</div>
+          <div className="product-name">{props.name}</div>
+          <div className="rating">
+            <span>4.7</span>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+          </div>
+          <button className="buy-bttn-blue" style={{ width: "100%", borderRadius: "6px" }}>Add To Cart</button>
         </div>
-        <div className="price">{props.price}{props.price< 100? ".00": ""} $</div>
       </div>
     </a>
   );
